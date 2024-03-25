85b9b2c4bac08b20ed54b325981c979aa7f5448e
Sahil-Mhatre14 <sahil81623@gmail.com>
2024-03-25 17:38:13 +0530

    implement bottombar, leftsidebar & rightsidebar

--- components/shared/Bottombar.tsx
+++ components/shared/Bottombar.tsx
@@ -1,5 +1,38 @@
-function Bottombar () {
-    return <h1>Bottombar</h1>
+"use client"
+
+import { sidebarLinks } from "@/constants";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+function Bottombar() {
+  const pathname = usePathname();
+
+  return (
+    <section className="bottombar">
+      <div className="bottombar_container">
+        {sidebarLinks?.map((link) => {
+          const isActive =
+            pathname === link?.route || pathname?.includes(link?.route);
+          return (
+            <Link
+              href={link?.route}
+              key={link?.label}
+              className={`bottombar_link ${isActive && "bg-primary-500"}`}
+            >
+              <Image
+                src={link?.imgURL}
+                alt={link?.label}
+                width={24}
+                height={24}
+              />
+              <p className="text-subtle-medium text-light-1 max-sm:hidden">{link?.label?.split(" ")?.[0]}</p>
+            </Link>
+          );
+        })}
+      </div>
+    </section>
+  );
 }
 
-export default Bottombar
+export default Bottombar;
--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -1,4 +1,5 @@
 import { OrganizationSwitcher, SignOutButton, SignedIn } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
@@ -26,7 +27,7 @@ const Topbar = () => {
           </SignedIn>
         </div>
         <OrganizationSwitcher hidePersonal={false}
-          appearance={{ elements: { organizationSwitcherTrigger: "py-2 px-4" } }}
+          appearance={{ baseTheme: dark, elements: { organizationSwitcherTrigger: "py-2 px-4" } }}
         />
       </div>
     </nav>
