8d3512f6c28cd4ee38d5cc9f175b5e34cf3199bd
Sahil-Mhatre14 <sahil81623@gmail.com>
2024-04-06 22:34:13 +0530

    whitelist uploadthing

--- middleware.ts
+++ middleware.ts
@@ -1,24 +1,16 @@
+// Resource: https://clerk.com/docs/nextjs/middleware#auth-middleware
+// Copy the middleware code as it is from the above resource
+
 import { authMiddleware } from "@clerk/nextjs";
- 
-// See https://clerk.com/docs/references/nextjs/auth-middleware
-// for more information about configuring your Middleware
- 
+
 export default authMiddleware({
-  // Allow signed out users to access the specified routes:
-  publicRoutes: ['/', '/api/webhook/clerk'],
-  ignoredRoutes: ['/api/webhook/clerk']
-  // Prevent the specified routes from accessing
-  // authentication information:
-  // ignoredRoutes: ['/no-auth-in-this-route'],
+  // An array of public routes that don't require authentication.
+  publicRoutes: ["/api/webhook/clerk", "/api/uploadthing"],
+
+  // An array of routes to be ignored by the authentication middleware.
+  ignoredRoutes: ["/api/webhook/clerk"],
 });
- 
+
 export const config = {
-  matcher: [
-    // Exclude files with a "." followed by an extension, which are typically static files.
-    // Exclude files in the _next directory, which are Next.js internals.
- 
-    "/((?!.+\\.[\\w]+$|_next).*)",
-    // Re-include any files in the api or trpc folders that might have an extension
-    "/(api|trpc)(.*)"
-  ]
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
