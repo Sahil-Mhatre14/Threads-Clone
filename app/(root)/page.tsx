3a265af3566011263335f830ba420e8f659fccba
Sahil-Mhatre14 <sahil81623@gmail.com>
2024-04-07 22:13:37 +0530

    render threads

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,42 @@
-import { UserButton } from '@clerk/nextjs'
-import Image from 'next/image'
+import { fetchPosts } from "@/lib/actions/thread.actions";
+import { UserButton } from "@clerk/nextjs";
+import Image from "next/image";
+import { currentUser } from "@clerk/nextjs";
+import ThreadCard from "@/components/cards/ThreadCard";
 
-export default function Home() {
+export default async function Home() {
+  const user = await currentUser();
+
+  const result = await fetchPosts(1, 30);
+  console.log("Posts", result);
+
   return (
     <>
-      <h1 className='head-text text-left'>Home</h1>
+      <h1 className="head-text text-left">Home</h1>
+
+      <section className="mt-9 flex flex-col gap-10">
+        {result?.posts.length === 0 ? (
+          <p className="no-result">No threads found</p>
+        ) : (
+          <>
+            {result?.posts.map((post) => {
+              return (
+                <ThreadCard
+                  key={post._id}
+                  id={post._id}
+                  currentUserId={user.id || ''}
+                  parentId={post.parentId}
+                  content={post.text}
+                  author={post.author}
+                  community={post.community}
+                  createdAt={post.createdAt}
+                  comments={post.children}
+                />
+              );
+            })}
+          </>
+        )}
+      </section>
     </>
-  )
+  );
 }
