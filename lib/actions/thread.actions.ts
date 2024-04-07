4f44e522625a8e6c89a71073acf7b5f38fecb88b
Sahil-Mhatre14 <sahil81623@gmail.com>
2024-04-07 22:15:35 +0530

    add new actions

--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -18,23 +18,129 @@ export default async function createThread({
   communityId,
   path,
 }: Params) {
+  try {
+    connectToDB();
 
-    try {
-        connectToDB();
+    const createdThread = await Thread.create({
+      text,
+      author,
+      community: null,
+    });
 
-        const createdThread = await Thread.create({
-          text,
-          author,
-          community: null,
-        });
-      
-        await User.findByIdAndUpdate(author, {
-          $push: { threads: createdThread._id },
-        });
-      
-        revalidatePath(path)
-    } catch(error: any) {
-        throw new Error('Error while creating a new thread', error.message)
-    }
- 
+    await User.findByIdAndUpdate(author, {
+      $push: { threads: createdThread._id },
+    });
+
+    revalidatePath(path);
+  } catch (error: any) {
+    throw new Error("Error while creating a new thread", error.message);
+  }
 }
+
+export const fetchPosts = async (pageNumber = 1, pageSize = 20) => {
+  try {
+    connectToDB();
+
+    const skipAmount = (pageNumber - 1) * pageSize;
+
+    const postsQuery = Thread.find({
+      parentId: { $in: [null, undefined] },
+    })
+      .sort({ createdAt: "desc" })
+      .skip(skipAmount)
+      .limit(pageSize)
+      .populate({ path: "author", model: User })
+      .populate({
+        path: "children",
+        populate: {
+          path: "author",
+          model: User,
+          select: "_id name parentId image",
+        },
+      });
+
+    const totalPostsCount = await Thread.countDocuments({
+      parentId: { $in: [null, undefined] },
+    });
+
+    const posts = await postsQuery.exec();
+
+    const isNext = totalPostsCount > skipAmount + posts.length;
+
+    return { posts, isNext };
+  } catch (error: any) {
+    console.log("Error while fetching threads", error.message);
+  }
+};
+
+export const fetchThreadById = async (id: string) => {
+  try {
+    connectToDB();
+
+    const thread = Thread.findById(id)
+      .populate({
+        path: "author",
+        model: User,
+        select: "_id id name image",
+      })
+      .populate({
+        path: "children",
+        populate: [
+          {
+            path: "author",
+            model: User,
+            select: "_id id name image",
+          },
+          {
+            path: "children",
+            model: Thread,
+            populate: {
+              path: "author",
+              model: User,
+              select: "_id id name image",
+            },
+          },
+        ],
+      })
+      .exec();
+
+    return thread;
+  } catch (error: any) {
+    throw new Error(
+      "Something went wrong while fetching thread",
+      error.message
+    );
+  }
+};
+
+export const addCommentToThread = async (
+  threadId: string,
+  commentText: string,
+  userId: string,
+  path: string
+) => {
+  try {
+    connectToDB();
+    const originalThread = await fetchThreadById(threadId);
+
+    if (!originalThread) {
+      throw new Error("Thread not found");
+    }
+
+    const commentThread = new Thread({
+      text: commentText,
+      author: userId,
+      parentId: threadId,
+    });
+
+    const savedCommentThread = await commentThread.save();
+
+    originalThread.children.push(savedCommentThread._id);
+
+    await originalThread.save();
+
+    revalidatePath(path);
+  } catch (error: any) {
+    console.log("Error while adding comment to thread", error.message);
+  }
+};
