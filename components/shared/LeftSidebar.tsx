f895a005cc8c562661c0b84fb505eae335f659c1
Sahil-Mhatre14 <sahil81623@gmail.com>
2024-04-14 13:27:52 +0530

    update logic to show active tab

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -12,15 +12,15 @@ const LeftSidebar = () => {
   const pathname = usePathname();
 
   console.log("pathname", pathname);
-  
-  
 
   return (
     <section className="custom-scrollbar leftsidebar">
       <div className="flex w-full flex-1 flex-col gap-6 px-6">
         {sidebarLinks?.map((link) => {
           const isActive =
-            pathname === link?.route && pathname?.includes(link?.route);
+            (pathname.includes(link.route) && link.route.length > 1) ||
+            pathname === link.route;
+            
           return (
             <Link
               href={link?.route}
