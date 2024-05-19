92c9005f8aa8255b766496ae48fec02168dc3515
Sahil-Mhatre14 <sahil81623@gmail.com>
2024-05-19 12:18:13 +0530

    complete UserCard component

--- components/cards/UserCard.tsx
+++ components/cards/UserCard.tsx
@@ -1,5 +1,10 @@
+"use client"
+
 import Image from "next/image";
 import React from "react";
+import { Button } from "../ui/button";
+import { Router } from "next/router";
+import { useRouter } from "next/navigation";
 
 interface Props {
   id: string;
@@ -10,6 +15,8 @@ interface Props {
 }
 
 const UserCard = ({ id, name, username, imgUrl, personType }: Props) => {
+  const router = useRouter();
+
   return (
     <article className="user-card">
       <div className="user-card_avatar">
@@ -20,6 +27,18 @@ const UserCard = ({ id, name, username, imgUrl, personType }: Props) => {
           height={48}
           className="rounded-full"
         />
+        <div className="flex-1 text-ellipsis">
+          <h4 className="text-base-semibold text-light-1">{name}</h4>
+          <p className="text-small-medium text-gray-1">@{username}</p>
+        </div>
+        <Button
+          className="user-card_btn"
+          onClick={() => {
+            router.push(`/profile/${id}`);
+          }}
+        >
+          View
+        </Button>
       </div>
     </article>
   );
