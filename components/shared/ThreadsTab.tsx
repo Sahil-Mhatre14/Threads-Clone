6d2830b6b95552747118bf2e7e9202ff93d0539f
Sahil-Mhatre14 <sahil81623@gmail.com>
2024-05-25 13:24:23 +0530

    handle user as well as community account type

--- components/shared/ThreadsTab.tsx
+++ components/shared/ThreadsTab.tsx
@@ -2,6 +2,7 @@ import { fetchUserPosts } from "@/lib/actions/user.actions";
 import { redirect } from "next/navigation";
 import React from "react";
 import ThreadCard from "../cards/ThreadCard";
+import { fetchCommunityPosts } from "@/lib/actions/community.actions";
 
 interface Props {
   currentUserId: string;
@@ -10,10 +11,17 @@ interface Props {
 }
 
 const ThreadsTab = async ({ currentUserId, accountId, accountType }: Props) => {
-  let result = await fetchUserPosts(accountId);
+  let result : any
+
+  if (accountType === 'Community') {
+    result = await fetchCommunityPosts(accountId)
+  } else {
+    result = await fetchUserPosts(accountId)
+  }
 
   if (!result) redirect("/");
-
+  console.log("Results", result);
+  
   return (
     <section className="mt-9 flex flex-col gap-10">
       {result.threads.map((thread: any) => (
@@ -32,7 +40,11 @@ const ThreadsTab = async ({ currentUserId, accountId, accountType }: Props) => {
                   id: thread.author.id,
                 }
           }
-          community={thread.community}
+          community={
+            accountType === "Community"
+              ? { name: result.name, id: result.id, image: result.image }
+              : thread.community
+          }
           createdAt={thread.createdAt}
           comments={thread.children}
         />
