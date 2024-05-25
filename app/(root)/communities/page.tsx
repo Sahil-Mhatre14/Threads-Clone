ddbda56a45b4474267ea99de22c62aea020f4873
Sahil-Mhatre14 <sahil81623@gmail.com>
2024-05-25 17:33:20 +0530

    make communitites search page

--- app/(root)/communities/page.tsx
+++ app/(root)/communities/page.tsx
@@ -1,11 +1,53 @@
-import React from 'react'
+import CommunityCard from "@/components/cards/CommunityCard";
+import UserCard from "@/components/cards/UserCard";
+import { fetchCommunities } from "@/lib/actions/community.actions";
+import { fetchUser, fetchUsers } from "@/lib/actions/user.actions";
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
-const Page = () => {
+const Page = async () => {
+  const user = await currentUser();
+
+  console.log("Current user", user);
+
+  if (!user) return null;
+
+  const userInfo = await fetchUser(user.id);
+
+  if (!userInfo?.onboarded) redirect("/onboarding");
+
+  const results = await fetchCommunities({
+    searchString: "",
+    pageNumber: 1,
+    pageSize: 10,
+    sortBy: "desc",
+  });
+
   return (
     <section>
-        <h1>Communities Page</h1>
-    </section>
-  )
-}
+      <h1 className="head-text text-left">Communities Page</h1>
 
-export default Page
+      <div className="mt-14 flex flex-col gap-9">
+        {results.communities.length === 0 ? (
+          <p>No users found</p>
+        ) : (
+          <>
+            {results.communities.map((community) => (
+              <CommunityCard
+                key={community.id}
+                id={community.id}
+                name={community.name}
+                username={community.username}
+                imgUrl={community.image}
+                bio={community.bio}
+                members={community.members}
+              />
+            ))}
+          </>
+        )}
+      </div>
+    </section>
+  );
+};
+
+export default Page;
